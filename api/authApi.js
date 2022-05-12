753967b6a83f58b43f238b4b1d5b7dccae5d77d8
thasuohm <thasuohm@gmail.com>
2022-05-12 23:58:06 +0700

    send cookie back after login

--- api/authApi.js
+++ api/authApi.js
@@ -1,26 +1,30 @@
-const router = require("express").Router(),
-  jwt = require("jsonwebtoken");
-passport = require("passport");
-const UserModel = require("../models/user");
-const { create, read, update, remove ,readWithPages } = require("../common/crud");
-
+const router = require('express').Router(),
+  jwt = require('jsonwebtoken')
+passport = require('passport')
+const UserModel = require('../models/user')
+const {create, read, update, remove, readWithPages} = require('../common/crud')
 
 /* POST login. */
 router
-  .post("/login", (req, res, next) => {
-    passport.authenticate("local", { session: false }, (err, user, info) => {
-      if (err) return next(err);
+  .post('/login', (req, res, next) => {
+    passport.authenticate('local', {session: false}, (err, user, info) => {
+      if (err) return next(err)
       if (user) {
-        const payload = { email: user.email ,role:user.role};
-        const token = jwt.sign(payload, "your_jwt_secret");
-        return res.json({ user, token });
+        const payload = {email: user.email, role: user.role}
+        const token = jwt.sign(payload, 'your_jwt_secret')
+        res.cookie('jwt', token, {
+          // secure: true,  // set secure ของ cookie ปกติมักใช้ใน production
+          maxAge: 3 * 24 * 60 * 60 * 1000,
+          httpOnly: true,
+        })
+        return res.json({user})
       } else {
-        return res.status(422).json(info);
+        return res.status(422).json(info)
       }
-    })(req, res, next);
+    })(req, res, next)
   })
-  .post("/register",create(UserModel))
+  .post('/register', create(UserModel))
   .get('/profile', (req, res, next) => {
-    res.send(req.user);
-});
-module.exports = router;
+    res.send(req.user)
+  })
+module.exports = router
