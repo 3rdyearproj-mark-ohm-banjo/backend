4b8f942e00f9493d10f19c22e1d8109aaaf93808
thasuohm <thasuohm@gmail.com>
2022-05-12 22:52:40 +0700

    add cookie parser to get jwt in cookie from client

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const passport = require("passport");
+const express = require('express')
+const bodyParser = require('body-parser')
+const passport = require('passport')
 // const path = require('path');
 // const logger = require('morgan');
 //const api = require('./api/test');
-const bSApi = require("./api/bookShelfApi");
-const pubApi = require("./api/publisherApi");
-const typeApi = require("./api/typeApi");
-const authApi = require("./api/authApi");
-const userApi = require("./api/userApi");
-const userBookShelfApi = require("./api/userBookShelfApi");
+const bSApi = require('./api/bookShelfApi')
+const pubApi = require('./api/publisherApi')
+const typeApi = require('./api/typeApi')
+const authApi = require('./api/authApi')
+const userApi = require('./api/userApi')
+const userBookShelfApi = require('./api/userBookShelfApi')
+const cookieParser = require('cookie-parser')
 
-const { notFound } = require("./common/middleware");
-const cors = require("cors");
+const {notFound} = require('./common/middleware')
+const cors = require('cors')
 //const multer = require('multer')
 //const upload = multer()
-require("./configs/passport");
-const app = express();
+require('./configs/passport')
+const app = express()
 
 app
+  .use(cookieParser())
   .use(
     cors({
-      origin: ["http://localhost:3000"],
-      methods: ["GET", "POST", "PUT", "DELETE"],
+      origin: ['http://localhost:3000'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
       credentials: true,
     })
   )
@@ -35,19 +37,19 @@ app
   //     extended:true
   // }))
 
-  .use("/api/bookShelf", bSApi)
-  .use("/api/publisher", pubApi)
-  .use("/api/type", typeApi)
-  .use("/api/", authApi)
-  .use("/api/user", passport.authenticate("jwt", { session: false }), userApi)
+  .use('/api/bookShelf', bSApi)
+  .use('/api/publisher', pubApi)
+  .use('/api/type', typeApi)
+  .use('/api/', authApi)
+  .use('/api/user', passport.authenticate('jwt', {session: false}), userApi)
   .use(
-    "/api/user",
-    passport.authenticate("jwt", { session: false }),
+    '/api/user',
+    passport.authenticate('jwt', {session: false}),
     userBookShelfApi
   )
 
   //.use('/api/book', api)
 
-  .use(notFound);
+  .use(notFound)
 
-module.exports = app;
+module.exports = app
--- configs/passport.js
+++ configs/passport.js
@@ -1,72 +1,79 @@
-const passport = require("passport"),
-  LocalStrategy = require("passport-local").Strategy;
-const passportJWT = require("passport-jwt"),
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
-const UserModel = require("../models/user");
+const passport = require('passport'),
+  LocalStrategy = require('passport-local').Strategy
+const passportJWT = require('passport-jwt'),
+  JWTStrategy = passportJWT.Strategy
+const UserModel = require('../models/user')
+
+const cookieExtractor = (req) => {
+  let jwt = null
+
+  if (req && req.cookies) {
+    jwt = req.cookies['jwt']
+  }
+
+  return jwt
+}
 
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "email",
-      passwordField: "password",
+      usernameField: 'email',
+      passwordField: 'password',
     },
     async (email, password, cb) => {
       //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
-
-    //   return UserModel.findOne({ email })
-    //     .then((user) => {
-    //         const validate = await user.isValidPassword(password)
-    //       if (!user) {
-    //         return cb(null, false, { message: "Incorrect email or password." });
-
-    //       }  if (!user.isValidPassword(password)) {
-    //         return cb(null, false, { message: "Incorrect email or password." });
-    //       }
-    //       console.log(user)
-    //       console.log(password)
-    //       console.log(validate)
-    //       return cb(null, user, { message: "Logged In Successfully" });
-    //     })
-    //     .catch((err) => cb(err));
-    try {
-        const user = await UserModel.findOne({ email });
+      //   return UserModel.findOne({ email })
+      //     .then((user) => {
+      //         const validate = await user.isValidPassword(password)
+      //       if (!user) {
+      //         return cb(null, false, { message: "Incorrect email or password." });
+      //       }  if (!user.isValidPassword(password)) {
+      //         return cb(null, false, { message: "Incorrect email or password." });
+      //       }
+      //       console.log(user)
+      //       console.log(password)
+      //       console.log(validate)
+      //       return cb(null, user, { message: "Logged In Successfully" });
+      //     })
+      //     .catch((err) => cb(err));
+      try {
+        const user = await UserModel.findOne({email})
 
         if (!user) {
-          return cb(null, false, { message: 'User not found' });
+          return cb(null, false, {message: 'User not found'})
         }
 
-        const validate = await user.isValidPassword(password);
+        const validate = await user.isValidPassword(password)
 
         if (!validate) {
-          return cb(null, false, { message: 'Wrong Password' });
+          return cb(null, false, {message: 'Wrong Password'})
         }
 
-        return cb(null, user, { message: 'Logged in Successfully' });
+        return cb(null, user, {message: 'Logged in Successfully'})
       } catch (error) {
-        return cb(error);
+        return cb(error)
       }
     }
   )
-);
+)
 
 passport.use(
   new JWTStrategy(
     {
-      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: "your_jwt_secret",
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: 'your_jwt_secret',
     },
     (jwtPayload, cb) => {
       //find the user in db if needed. This functionality may be omitted if you store everything you'll need in JWT payload.
 
-      const email = jwtPayload.email;
-      return UserModel.findOne({ email })
+      const email = jwtPayload.email
+      return UserModel.findOne({email})
         .then((user) => {
-          return cb(null, user);
+          return cb(null, user)
         })
         .catch((err) => {
-          return cb(err);
-        });
+          return cb(err)
+        })
     }
   )
-);
+)
