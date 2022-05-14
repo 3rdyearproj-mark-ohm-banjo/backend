ed4767fdd548ebff47a06f531aa1a06b998e6032
thasuohm <thasuohm@gmail.com>
2022-05-14 13:10:58 +0700

    set env

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
 const app = require('./app')
 const mongoose = require('mongoose') // อาจเกิด error
-mongoose.connect(
-  'mongodb://thasuohm:AHG3KpdHlkBNgjhb@int365-shard-00-00.lufhw.mongodb.net:27017,int365-shard-00-01.lufhw.mongodb.net:27017,int365-shard-00-02.lufhw.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-mz4k34-shard-0&authSource=admin&retryWrites=true&w=majority',
-  //'mongodb://localhost:27017/Share_My_Book'
-  {useNewUrlParser: true}
-) // ส่วนนี้เป็น Option ถ้าไม่ใส่จะ warning ว่าการ connect mongodb ด้วย url แบบ string ในอนาคตจะ depreacated แล้ว (ซึ่งไม่ใส่ก็ได้ แต่จะมี warning แค่นั้น)
+const config = require('config')
+const DB_CONFIG = config.get('DB_CONFIG')
+
+mongoose.connect(DB_CONFIG.DB_URI, {useNewUrlParser: true}) // ส่วนนี้เป็น Option ถ้าไม่ใส่จะ warning ว่าการ connect mongodb ด้วย url แบบ string ในอนาคตจะ depreacated แล้ว (ซึ่งไม่ใส่ก็ได้ แต่จะมี warning แค่นั้น)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-  console.log(`Start server at port ${PORT}.`)
+  console.log(
+    `Start server at port ${PORT}. with mode: ${process.env.NODE_ENV}`
+  )
 })
-
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World')
-// })
-
-// app.listen(3000, () => {
-//   console.log('Start server at port 3000.')
-// })
