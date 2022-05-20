cad0b287b30c60ebc7675d0415930788e0022d3c
thasuohm <thasuohm@gmail.com>
2022-05-20 15:00:43 +0700

    return user in login to decrease request

--- api/authApi.js
+++ api/authApi.js
@@ -9,24 +9,39 @@ const SECRET = config.get('SECRET_KEY')
 /* POST login. */
 router
   .post('/login', (req, res, next) => {
-    passport.authenticate('local', {session: false}, (err, user, info) => {
-      if (err) return next(err)
-      if (user) {
-        const payload = {email: user.email, role: user.role}
-        const token = jwt.sign(payload, SECRET, {
-          expiresIn: '3d' 
-     })
-        res.cookie('jwt', token, {
-          // secure: true,  // set secure ของ cookie ปกติมักใช้ใน production
-          maxAge: 3 * 24 * 60 * 60 * 1000,
-          httpOnly: true,
-        })
-        //return res.json({user})
-        return res.json({message:"login success"})
-      } else {
-        return res.status(422).json(info)
+    passport.authenticate(
+      'local',
+      {session: false},
+      async (err, user, info) => {
+        if (err) return next(err)
+        if (user) {
+          const payload = {email: user.email, role: user.role}
+          const token = jwt.sign(payload, SECRET, {
+            expiresIn: '3d',
+          })
+          res.cookie('jwt', token, {
+            // secure: true,  // set secure ของ cookie ปกติมักใช้ใน production
+            maxAge: 3 * 24 * 60 * 60 * 1000,
+            httpOnly: true,
+          })
+          //return res.json({user})
+          const userData = await UserModel.find({email: user.email}).populate({
+            path: 'donationHistory',
+            populate: {
+              path: 'book',
+              model: 'books',
+              populate: {
+                path: 'bookShelf',
+                model: 'bookshelves',
+              },
+            },
+          })
+          return res.status(200).json({message: 'login success', user: userData[0]})
+        } else {
+          return res.status(422).json(info)
+        }
       }
-    })(req, res, next)
+    )(req, res, next)
   })
   .post('/register', roleUserOnly(), create(UserModel))
   .get('/profile', (req, res, next) => {
@@ -39,11 +54,11 @@ router
     }
     return res.status(401).json('you are not logged in')
   })
-  function roleUserOnly(){
-    return (req,res,next)=>{
-      req.body = {... req.body,role:"user"}
-      next()
-    }
+function roleUserOnly() {
+  return (req, res, next) => {
+    req.body = {...req.body, role: 'user'}
+    next()
   }
+}
 
 module.exports = router
