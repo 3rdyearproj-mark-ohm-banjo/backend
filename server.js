c1c09407d37748e3b78fe4ab00fcec76ae19a12a
punthanat <punthanat@hotmail.com>
2022-05-20 22:20:40 +0700

    [Fix] config port

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const app = require('./app')
 const mongoose = require('mongoose') // อาจเกิด error
 const config = require('config')
 const DB_CONFIG = config.get('DB_CONFIG')
+// const PORT = config.get('PORT')
 
-mongoose.connect(DB_CONFIG.DB_URI, {useNewUrlParser: true}) // ส่วนนี้เป็น Option ถ้าไม่ใส่จะ warning ว่าการ connect mongodb ด้วย url แบบ string ในอนาคตจะ depreacated แล้ว (ซึ่งไม่ใส่ก็ได้ แต่จะมี warning แค่นั้น)
+mongoose.connect(DB_CONFIG.DB_URI, { useNewUrlParser: true }) // ส่วนนี้เป็น Option ถ้าไม่ใส่จะ warning ว่าการ connect mongodb ด้วย url แบบ string ในอนาคตจะ depreacated แล้ว (ซึ่งไม่ใส่ก็ได้ แต่จะมี warning แค่นั้น)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
